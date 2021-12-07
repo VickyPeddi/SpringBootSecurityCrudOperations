525abd12244a957350275e29cdd29b45e7c76a7a
NandaKishore.Peddi <vickypeddi9@gmail.com>
2021-12-07 20:44:12 +0530

    this is token completed profile

--- src/main/java/com/example/nanda/config/Mysecurityconfig.java
+++ src/main/java/com/example/nanda/config/Mysecurityconfig.java
@@ -5,13 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -27,22 +27,15 @@ public class Mysecurityconfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                // Our public endpoints
-                .antMatchers("generatetoken").permitAll()
-                .antMatchers(HttpMethod.GET, "/testuser/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/testuser/adduser").permitAll()
-                .antMatchers("/user/**").permitAll()
-        // Our private endpoints
-        ;
-        http.cors().and().csrf().disable();
+        http.authorizeRequests().antMatchers("/take/**", "/user/**", "/testuser/**").permitAll().anyRequest().authenticated();
+
     }
 
-//	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
-//	@Override
-//	public AuthenticationManager authenticationManagerBean() throws Exception {
-//		return super.authenticationManagerBean();
-//	}
+	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 
 
     @Bean
@@ -62,6 +55,6 @@ public class Mysecurityconfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
 }
--- src/main/java/com/example/nanda/controller/Testcontroller.java
+++ src/main/java/com/example/nanda/controller/Testcontroller.java
@@ -4,39 +4,47 @@ import com.example.nanda.model.Jwtrequest;
 import com.example.nanda.securityhepler.CustomUserdetailsservice;
 import com.example.nanda.util.Jwtutil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
+@RequestMapping("/take/")
 public class Testcontroller {
     @Autowired
     private Jwtutil jwtutil;
-
+    @Autowired
+    AuthenticationManager manager;
     @Autowired
 
     private CustomUserdetailsservice userderCustomUserdetailsservice;
 
+    @RequestMapping("/taketoken")
+    public ModelAndView gototoken(@ModelAttribute("jwtrequest") Jwtrequest jwtrequest) {
+
+        return new ModelAndView("Testpage");
+    }
+
     @PostMapping("/generatetoken")
-    public String returntest(Principal principal) {
-
+    public ModelAndView returntest(@ModelAttribute("jwtrequest") Jwtrequest jwtrequest) {
         try {
-//            UserDetails details = userderCustomUserdetailsservice.loadUserByUsername(rJwtrequest.getUsername());
-            String name = principal.getName();
-            System.out.println(name);
-            String generateToken = jwtutil.generateToken(name);
 
-            System.out.println("generatetoken " + generateToken);
-            return generateToken;
+            Authentication authenticate = manager.authenticate(new UsernamePasswordAuthenticationToken(jwtrequest.getUsername(), jwtrequest.getPassword()));
+
+
         } catch (Exception e) {
-            String localizedMessage = e.getLocalizedMessage();
-            return localizedMessage;
 
         }
+
+        UserDetails userDetails = userderCustomUserdetailsservice.loadUserByUsername(jwtrequest.getUsername());
+        String token = jwtutil.generateToken(userDetails.getUsername());
+        return new ModelAndView("Testpage", "token",token );
 
     }
 }
